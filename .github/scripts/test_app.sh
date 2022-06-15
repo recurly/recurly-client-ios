43bb4926ff9538a7ae2fab9947cc7b657b466dd0
Mark Roman <mroman@recurly.com>
2022-06-15 08:45:30 -0600

    added workspace

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,7 @@
 
 set -eo pipefail
 
-xcodebuild  -scheme ContainerApp \
+xcodebuild  -workspace project.xcworkspace \
+	    -scheme ContainerApp \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
