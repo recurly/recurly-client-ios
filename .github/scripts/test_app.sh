b625bdd33fce7d3756567075bc0064b9c82cf074
Mark Roman <mroman@recurly.com>
2022-06-15 11:41:59 -0600

    trying different flag

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 
 xcodebuild  -project RecurlySDK-iOS.xcodeproj \
 	    -scheme RecurlySDK-iOS \
-            -destination 'platform=iOS\ Simulator,OS=15.2,name=iPhone 13' \
+            -destination 'platform=iOS Simulator,OS=15.2,name=iPhone 13' \
 	    -only-testing RecurlySDK-iOSTests
