e2e8d68f2d4ddc966a26b7e9f775b7f7e3d6482e
Mark Roman <mroman@recurly.com>
2022-06-15 09:52:35 -0600

    debugging

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -7,4 +7,4 @@ xcodebuild  -project RecurlySDK-iOS.xcodeproj \
 	    -scheme RecurlySDK-iOS \
 	    -sdk iphonesimulator \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 13 \
-            clean test | xcpretty
+            clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_ALLOWED=NO test | xcpretty
