f9181d51cb275a6a9fd04e231b46691d58d2a8d1
Mark Roman <mroman@recurly.com>
2022-06-15 09:40:14 -0600

    debugging

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -6,5 +6,5 @@ set -eo pipefail
 xcodebuild  -project RecurlySDK-iOS.xcodeproj \
 	    -scheme RecurlySDK-iOSTests \
 	    -sdk iphonesimulator \
-            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 15 \
+            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 13 \
             clean test | xcpretty
