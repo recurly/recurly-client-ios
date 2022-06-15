dadf07a8f9a04796badd6a4814412a2ad2884cec
Mark Roman <mroman@recurly.com>
2022-06-15 08:50:14 -0600

    switched to project

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,7 +3,7 @@
 
 set -eo pipefail
 
-xcodebuild  -workspace RecurlySDK-iOS \
+xcodebuild  -project RecurlySDK-iOS.xcodeproj \
 	    -scheme ContainerApp \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
