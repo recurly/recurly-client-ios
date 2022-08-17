e67fd2a7c97ff336d095d57d58cb9164f08b594b
Mark Roman <mroman@recurly.com>
2022-08-17 10:37:59 -0600

    updated swift package file

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "recurly-client-ios",
+    name: "RecurlySDK-iOS",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "recurly-client-ios",
-            targets: ["recurly-client-ios"]),
+            name: "RecurlySDK-iOS",
+            targets: ["RecurlySDK-iOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "recurly-client-ios",
+            name: "RecurlySDK-iOS",
             dependencies: []),
         .testTarget(
-            name: "recurly-client-iosTests",
+            name: "RecurlySDK-iOSTests",
             dependencies: ["recurly-client-ios"]),
     ]
 )
