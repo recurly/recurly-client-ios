2e1c49a394fc95a6411d20773cd9378ed55927e7
Mark Roman <mroman@recurly.com>
2022-08-29 10:46:47 -0600

    added path to package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RecurlySDK-iOS",
-            dependencies: []),
+            dependencies: [],
+            path: "RecurlySDK-iOS"),
         .testTarget(
             name: "RecurlySDK-iOSTests",
             dependencies: ["RecurlySDK-iOS"],
