2c7df97047b7590dfe3c76bfa98e2895b379a22b
Mark Roman <mroman@recurly.com>
2022-08-29 10:40:56 -0600

    fixed closing bracket

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "RecurlySDK-iOSTests",
-            dependencies: ["RecurlySDK-iOS"]),
+            dependencies: ["RecurlySDK-iOS"],
             path: "RecurlySDK-iOSTests"),
     ]
 )
