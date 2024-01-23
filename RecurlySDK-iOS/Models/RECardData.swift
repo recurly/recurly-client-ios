1f7da2315e03eeb926917da92ac4be200d4ed512
xZenon <xzenon@gmail.com>
2024-01-23 17:29:50 +0200

    Provide initializer for RECardData

--- RecurlySDK-iOS/Models/RECardData.swift
+++ RecurlySDK-iOS/Models/RECardData.swift
@@ -10,11 +10,29 @@ import Foundation
 /// Recurly Card Data Model
 public struct RECardData: Codable {
     /// Credit Card number
-    var number: String = ""
+    var number: String
     /// Expiration Month
-    var month: String = ""
+    var month: String
     /// Expiration Year
-    var year: String = ""
+    var year: String
     /// Security Code
-    var cvv: String = ""
+    var cvv: String
+
+    enum CodingKeys: String, CodingKey {
+        case number
+        case month
+        case year
+        case cvv
+    }
+    
+    public init(number: String = "",
+                month: String = "",
+                year: String = "",
+                cvv: String = "") {
+        
+        self.number = number
+        self.month = month
+        self.year = year
+        self.cvv = cvv
+    }
 }
