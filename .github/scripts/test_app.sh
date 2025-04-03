80b9f4dff8a5bf1d6064ab7cbf008d63aa4a5614
Christopher Rogers <chrissrogers@gmail.com>
2025-04-03 14:12:22 -0700

    Moves test script

--- .github/scripts/test_app.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -project RecurlySDK-iOS.xcodeproj \
-	       -scheme RecurlySDK-iOS \
-           -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' \
-	       -only-testing RecurlySDK-iOSTests
--- RecurlySDK-iOSTests/RecurlySDK-ApplePayTests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RecurlySDK-ApplePayTests.swift
-//  RecurlySDK-iOSTests
-//
-//  Created by George Andrew Shoemaker on 8/24/22.
-//
-
-import Foundation
